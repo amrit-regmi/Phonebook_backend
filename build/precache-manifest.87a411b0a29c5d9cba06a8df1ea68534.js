b8779b80a8101ede23a17d7408967cdf6451bc03
amrit <rit.regmi@gmail.com>
2020-07-22 15:47:45 +0300

    Deplyoing Frontend

---
+++ build/precache-manifest.87a411b0a29c5d9cba06a8df1ea68534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a247dd90f27389582f37586b04f1632",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22cd83c11bd36e06e344",
+    "url": "/static/css/main.84473906.chunk.css"
+  },
+  {
+    "revision": "c8cfa160144a3957c934",
+    "url": "/static/js/2.50c0ef03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.50c0ef03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22cd83c11bd36e06e344",
+    "url": "/static/js/main.551abfbc.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
